c8fcfb5b9df70ccf0c712c6b044e3cbfd94cc8d0
Bastián Villanueva Meza <72043421+Bastian1212@users.noreply.github.com>
2023-08-10 11:58:57 -0400

    variables de entorno front

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log('VITE_APP_HOST:', process.env.VITE_APP_HOST);
+console.log('VITE_APP_PORT:', process.env.VITE_APP_PORT);
+
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
   plugins: [react()],
+  server :{
+    host: process.env.VITE_APP_HOST,
+    port: process.env.VITE_APP_PORT,
+  },
 })
